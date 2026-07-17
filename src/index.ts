3e4008798b61522db8a44fae5987adeb2d0e8b6a
uther <uther4ball@gmail.com>
2026-07-18 02:51:16 +0800

    Healthcheck

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 import 'dotenv/config';
+import express from 'express';
 import BotClient from './structures/BotClient.js'
+
+const app = express();
+const PORT = 3000;
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+})
+
+app.listen(PORT, () => {
+  console.log(`Health check server listening on port ${PORT}`);
+});
 
 BotClient.start(process.env.TOKEN);
